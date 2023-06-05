22bc2b5e9d57ba6257174a411f9bea1ccb0d1f94
sri1321 <srianjaneyulu.sightspectrum@gmail.com>
2023-06-05 18:21:21 +0530

    functions

---
+++ functions/functions.py
@@ -0,0 +1,31 @@
+##passing one argument
+##def my_function():
+##  print("Hello from a function")
+##
+##my_function()
+
+##passing no of arguments
+##def my_function(fname):
+##    print(fname + " Refsnes")
+##
+##my_function("Emil")
+##my_function("Tobias")
+##my_function("Linus")
+
+
+##Expecing correct no of arguments
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil")
+
+
+
+
+
